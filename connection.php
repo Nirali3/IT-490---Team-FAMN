302c282b2955a6f93772913a5bde27b9a33ba888
Nirali Patel <np92@njit.edu>
2025-02-21 14:31:45 -0500

    modified to verify connection

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 $host = "100.69.138.84";
+$username = "backend_user";
 $password = "490AFMNprojectFinal";
 $database = "project490_db";
-$username = "backend_user";
 
-$con = new mysqli($host, $username, $password, $database);
+$con = new mysqli($host,$username,$password,$database);
 	
 if ($con->connect_error){
 	die("Failed to connect: " . $con->connect_error);

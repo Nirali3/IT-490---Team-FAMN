309cac0a5dd795dadd46b3cc28a057294add5225
Nirali Patel <np92@njit.edu>
2025-02-21 14:14:59 -0500

    made changes to the login.php: added validation for any errors

--- login.php
+++ login.php
@@ -3,20 +3,71 @@ session_start();
 
 include "connection.php";
 
-if (isset($_POST['login'])){
+$errors = [];
+
+$usernameRegex = /^[a-zA-Z][a-zA-Z0-9]{3,10}$/;
+$passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/; 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$username = trim($_POST['username']);
 	$password = trim($_POST['password']);
 
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
-	$username = con->real_escape_string($username);
-	$password = con->real_escape_string($password);
-
-	$usenamesql = "Select * FROM Login where username = '$username'";
-	$result = con->query($sql);
-
-	$passwordsql = "SELECT * FROM Login where password = '$password'";
-	$result = con->query($sql);
+	$stmt = $con->prepare("SELECT username, password FROM login WHERE username = ?");
+    	$stmt->bind_param("s", $username);
+    	$stmt->execute();
+    	$stmt->store_result();
+	
+	if ($stmt->num_rows > 0) {
+		$stmt->bind_result($hashed_password);
+		$stmt->fetch();
+		
+		if (password_verify($password, $hashed_password)) {
+			$_SESSION["username"] = $username;
+            		header("Location: homepage.save.php");
+			exit();
+		} else {
+			echo "Invalid password!";
+		}
+	} else {
+		echo "USER DOES NOT EXIST! PLEASE CREATE AN ACCOUNT FIRST";
+		header("Location: register.php");
+		exit();
+	}
+	$stmt->close();
 }
+$con->close();
+
+if (empty($username)){
+	$errors = "Username cannot be empty. Please enter:";
+}
+
+if (empty($password)){
+	$errors = "Password cannot be empty. Please enter:";
+}
+
+//validation
+
+function validateUsername($username){
+	if (!$usernameRegex($username)){
+		echo "INVALID USERNAME. PLEASE ENTER A UNIQUE USERNAME";
+	}
+	else {
+		echo "VALID USERNAME!";
+	}
+}
+
+function validatePassword($password){
+	if (!$passwordRegex($password)){
+		echo "INVALID PASSWORD. PASSWORD MUST HAVE A SPECIAL CHARACTER, NUMBER AND SHOULD BE 7 CHARACTERS LONG. PLEASE RE-ENTER";
+	}
+	else {
+		echo "VALID PASSWORD!";
+	}
+}
+
 ?>
 
 <!DOCTYPE HTML>
@@ -25,6 +76,7 @@ if (isset($_POST['login'])){
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>User Login</title>
+<h1> Login </h1>
 </head>
 
 <body>

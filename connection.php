10d1fe2b07ea82029f5ac043b547708384747b88
Nirali Patel <np92@njit.edu>
2025-03-03 13:13:52 -0500

    added new file:connection.php

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$host = "100.69.138.84";
+$password = "490AFMNprojectFinal";
+$database = "project490_db";
+$username = "backend_user";
+
+$con = mysqli_connect($host,$username,$password,$database);
+	
+if(mysqli_connect_errno()){
+	echo "Failed to connect: " . mysqli_connect_error();
+}
+else{
+	echo "Connection Successful";
+}
+
+?>

74daba8d0b967cba9193d6ec982e77a7275e8497
Nirali Patel <np92@njit.edu>
2025-03-04 11:44:33 -0500

    commented out the connection sucessful statement

--- connection.php
+++ connection.php
@@ -9,8 +9,7 @@ $con = mysqli_connect($host,$username,$password,$database);
 if(mysqli_connect_errno()){
 	echo "Failed to connect: " . mysqli_connect_error();
 }
-else{
-	echo "Connection Successful";
-}
-
+//else{
+//	echo "Connection Successful";
+//}
 ?>

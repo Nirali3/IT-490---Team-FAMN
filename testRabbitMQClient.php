a7b7b4da494e594ee49ff4b06870e1d174bc2c93
Michele Elsa Tomey <me368@njit.edu>
2025-03-04 12:21:10 -0500

    Updated code

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -1,29 +1,34 @@
 #!/usr/bin/php
 <?php
-require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
-{
-  $msg = $argv[1];
-}
-else
-{
-  $msg = "test message";
-}
-
-$request = array();
-$request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
-$request['message'] = $msg;
-$response = $client->send_request($request);
+try {
+	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+	
+	$username = isset($argv[1]) ? $argv[1] : "defaultUser";
+	$password = isset($argv[2]) ? $argv[2] : "defaultPass";
+	$msg = isset($argv[3]) ? $argv[3] : "test message";
+	$request = array();
+	$request['type'] = "login";
+	$request['username'] = $username;
+	$request['password'] = $password;
+	$request['message'] = $msg;
+	$response = $client->send_request($request);
 //$response = $client->publish($request);
 
-echo "client received response: ".PHP_EOL;
-print_r($response);
-echo "\n\n";
-
+	if ($response === false) {
+		echo "Error: No response from server.\n";
+	} elseif (isset($response['error'])) {
+		echo "Server Error: " . $response['error'] . "\n";
+	} else {
+		echo "Client received response: " . PHP_EOL;
+		print_r($response);
+		echo "\n\n";
+	}
+} catch (Exception $e) {
+	echo "RabbitMQ Error: " . $e->getMessage() . "\n";
+}
 echo $argv[0]." END".PHP_EOL;
+
+?>

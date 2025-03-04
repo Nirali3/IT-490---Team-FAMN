69810237076b58372f4d190200686bf39bad2696
Michele Elsa Tomey <me368@njit.edu>
2025-03-04 13:27:09 -0500

    Added some lines of code for the login function

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -1,15 +1,27 @@
 #!/usr/bin/php
 <?php
-require_once('path.inc');
+
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 function doLogin($username,$password)
 {
-    // lookup username in databas
-    // check password
-    return true;
+    $validUsers = [ 
+	"test" => "Am12345."
+	];
+
+	if (isset($validUsers[$username]) && $validUsers[$username] === $password {
+		return ["success" => true, "message" => "Login successful"];
+	}
+
     //return false if not valid
+
+    		return ["success" => false, "message" => "Invalid credentials"];
+}
+
+function doValidate($sessionId)
+{
+	return ["success" => true, "message" => "Session is valid"];
 }
 
 function requestProcessor($request)
@@ -26,6 +38,8 @@ function requestProcessor($request)
       return doLogin($request['username'],$request['password']);
     case "validate_session":
       return doValidate($request['sessionId']);
+    default:
+      return ["error" => "Unknown request type"];
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }

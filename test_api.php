3e0f285bfc8bd162294e2452ae66fc7292a6a0ed
me368 <me368@njit.edu>
2025-03-18 16:20:53 -0400

    added code to test mail

--- test_api.php
+++ test_api.php
@@ -6,10 +6,22 @@
 
 //echo "API Key: " . $_ENV['GOOGLE_API_KEY'];
 
-if (function_exists('mail')) {
-    echo "Mail function is enabled.";
+//if (function_exists('mail')) {
+//    echo "Mail function is enabled.";
+//} else {
+  //  echo "Mail function is not enabled on this server.";
+//}
+$to = "jj@gmail.com"; // Replace with your email
+$subject = "Test Email from PHP Mail()";
+$message = "This is a test email sent from the PHP mail() function.";
+$headers = "From: noreply@yourwebsite.com\r\n";
+$headers .= "Reply-To: noreply@yourwebsite.com\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+if (mail($to, $subject, $message, $headers)) {
+    echo "Test email sent successfully!";
 } else {
-    echo "Mail function is not enabled on this server.";
+    echo "Failed to send test email.";
 }
 ?>
 

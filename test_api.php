f480e11b63bf6b11bce978c2a6241640de703715
me368 <me368@njit.edu>
2025-03-18 15:42:55 -0400

    testing if mail() function is working

--- test_api.php
+++ test_api.php
@@ -1,7 +1,15 @@
 <?php
-require 'vendor/autoload.php';
+//require 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable('/var/www/html', 'api.env');
-$dotenv->load();
+//$dotenv = Dotenv\Dotenv::createImmutable('/var/www/html', 'api.env');
+//$dotenv->load();
 
-echo "API Key: " . $_ENV['GOOGLE_API_KEY'];
+//echo "API Key: " . $_ENV['GOOGLE_API_KEY'];
+
+if (function_exists('mail')) {
+    echo "Mail function is enabled.";
+} else {
+    echo "Mail function is not enabled on this server.";
+}
+?>
+

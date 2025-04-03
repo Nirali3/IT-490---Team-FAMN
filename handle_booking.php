03eaf121873f01a8e09fcf203e7ed766c170fd81
Farheen Ali <fza6@njit.edu>
2025-04-03 12:04:34 -0400

    debugging

--- handle_booking.php
+++ handle_booking.php
@@ -4,6 +4,8 @@ ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 session_start();
 
+$_SESSION['passenger_id'] = $passenger_id;
+
 include "connection.php";
 require 'vendor/autoload.php';
 

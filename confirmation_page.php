3fb0293d11d56e10fc3c0f50d981fa6f9797b7ce
Farheen Ali <fza6@njit.edu>
2025-04-03 20:29:49 -0400

    changed format for date for arrivalDate

--- confirmation_page.php
+++ confirmation_page.php
@@ -47,8 +47,12 @@ $booking_info = $_SESSION['booking_info'];
     <div id="flight-info">
         <p><strong>Airline:</strong> <?= htmlspecialchars($booking_info['airline']) ?></p>
         <p><strong>Departure:</strong> <?= htmlspecialchars($booking_info['departureAirport']) ?> at <?= htmlspecialchars($booking_info['departure']) ?></p>
-        <p><strong>Arrival:</strong> <?= htmlspecialchars($booking_info['destinationAirport']) ?> at <?= htmlspecialchars($booking_info['arrival']) ?></p>
-']) ?></p>
+        <p><strong>Arrival:</strong> <?= htmlspecialchars($booking_info['destinationAirport']) ?> at
+	    <?php
+		$arrivalDate = new DateTime($booking_info['arrival']);
+		echo $arrivalDate->format('m/d/Y');
+	    ?>
+	</p>
         <p><strong>Price per Ticket:</strong> $<?= htmlspecialchars($booking_info['price']) ?></p>
     </div>
 

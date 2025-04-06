b6ceae088f192b1dfde1ae2f1a33e9816ec78760
Nirali Patel <np92@njit.edu>
2025-04-06 13:04:16 -0400

    testing

--- handle_booking.php
+++ handle_booking.php
@@ -4,6 +4,11 @@ ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 session_start();
 
+$user_id = $_SESSION['user_id']
+if (!$user_id) {
+    die("You must be logged in to book a flight.");
+}
+
 include "connection.php";
 require 'vendor/autoload.php';
 

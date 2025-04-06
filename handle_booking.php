ffd2b601202425ba7bf9a4611a8542f05f88df45
Nirali Patel <np92@njit.edu>
2025-04-06 13:00:36 -0400

    testing

--- handle_booking.php
+++ handle_booking.php
@@ -12,9 +12,9 @@ $dotenv->load();
 
 $api_key = $_ENV['GOOGLE_API_KEY'];
 
-if (!isset($_SESSION['user_id'])) {
+/*if (!isset($_SESSION['user_id'])) {
 	die("Please log in to book flight" . var_export($_SESSION, true));
-}
+}*/
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {

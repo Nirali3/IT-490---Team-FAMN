a457d96d2ae80acb5354822848502b49a4c42831
Nirali Patel <np92@njit.edu>
2025-04-07 19:16:12 -0400

    testing

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -12,7 +12,7 @@ function doLogin($username,$password)
 	$username => $password];
 
 	if (isset($validUsers[$username]) && $validUsers[$username] === $password) {
-		return ["success" => true, "message" => "Login successful"];
+		return ["success" => true, "message" => "Login successful", "user_id" => $user_id];
 	}
 
     //return false if not valid

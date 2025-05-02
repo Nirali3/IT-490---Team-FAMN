38f491dbaf79931815d63ec45f5d7f1c749a6c01
Nirali Patel <np92@njit.edu>
2025-05-02 15:41:59 -0400

    added bootstrap library

--- login.php
+++ login.php
@@ -102,6 +102,7 @@ function validatePassword($password, $passwordRegex) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Login Page</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <style>
 	.error { color: red; }
 

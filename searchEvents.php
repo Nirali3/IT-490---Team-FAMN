59270d3be1f8e0f459eaceb1b22b94aaf91519c2
Nirali Patel <np92@njit.edu>
2025-05-04 17:08:28 -0400

    included bootstrap library

--- searchEvents.php
+++ searchEvents.php
@@ -62,6 +62,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['Location'])){
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Event Search</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <style>
 
 	body{

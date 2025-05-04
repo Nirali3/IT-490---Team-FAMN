00aae106993d5befda4f89326fa32e9a06837093
Nirali Patel <np92@njit.edu>
2025-05-04 16:52:05 -0400

    included bootstrap library

--- homepage.php
+++ homepage.php
@@ -21,6 +21,7 @@ $username = $loggedIn ? $_SESSION['username'] : "Guest";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Explore & Book | Your Event and Flight Tracker</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <style>
         /* Light Blue Theme Styling */

06b90ce5e35a05f369acef80ef473bc05b7c9fb7
Nirali Patel <np92@njit.edu>
2025-05-05 19:01:26 -0400

    included bootstrap library

--- indexSearchFlight.php
+++ indexSearchFlight.php
@@ -63,6 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['origin'], $_GET['destin
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flight Search</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <style>
         body {
